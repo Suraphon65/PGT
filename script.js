1cb6e46287f0a163f0930099520286d571598681
Suraphon65 <suraphon.th.65@ubu.ac.th>
2024-09-08 22:04:17 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+}
